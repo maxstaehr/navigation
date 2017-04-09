4fce5a5f48baf65dccfe3bc81900b22a98e21faa
maxstaehr <max.staehr@gmail.com>
2017-04-09 09:42:27 +0200

    sd

---
+++ +PF/Robot.m
@@ -0,0 +1,21 @@
+classdef Robot
+    %ROBOT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        transSpeedLimit = [0 2];
+        transSpeedAcc = [0 1];
+        transSpeedJerk = [0 1];
+        rotSpeed = [0 1];
+        rotSpeedAcc = [0 1];
+        rotSpeedJerk = [0 1];
+        width = 1;
+    end
+    
+    methods
+        
+        
+    end
+    
+end
+
---
+++ +PF/StateEstimator.m
@@ -0,0 +1,12 @@
+classdef StateEstimator
+    %STATEESTIMATOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
