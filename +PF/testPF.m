97775dfad4341217ea57618226f8c31bc604ee94
maxstaehr <max.staehr@gmail.com>
2017-05-01 11:55:22 +0200

    Hinzufügen aller Variablen

--- +PF/testPF.m
+++ +PF/testPF.m
@@ -1,10 +1,19 @@
+
 pf = PF.PF();
 vtrans = [1 0 0 ]';
+accx = 0;
+omega = 0;
+x = (0:pf.dt:10)*vtrans(1);
 vrot = [0 0 ]';
-pf.predict(vtrans, vrot);
-figure; hold on; grid on;
+close all; figure; 
+subplot(2, 2, 1);
+hold on; grid on;
 ax = gca;
-for i=1:100
-    pf.step(vtrans, vrot);
-    pf.plotPaticle(ax);
+subplot(2, 2, 2);
+axhist = gca;
+subplot(2, 2, 4);
+axhist2 = gca;
+for i=1:length(x)
+    pf.step(vtrans, vrot, accx, omega, [x(i) 0 0]);
+    pf.plotPaticle(ax, axhist, axhist2);
 end
